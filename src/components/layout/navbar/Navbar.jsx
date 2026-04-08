45d320ae9d74a2ddc101042d358b2904f1c81705
Romeo-Martial <104857205+Romeo-Martial@users.noreply.github.com>
2026-04-08 22:17:04 +0200

    fix some import mismatches

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import LanguageSwitcher from "../../common/languageSwitcher/LanguageSwitcher";
+import LanguageSwitcher from "../../common/LanguageSwitcher/LanguageSwitcher";
 import Button from "../../common/button/Button";
 import "./Navbar.css";
 
